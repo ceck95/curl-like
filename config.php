3735827576da2acc1a7b5c3578e96338afdecd7f
Trần Văn Nhứt <tranvannhut4495@gmail.com>
2016-03-01 20:50:38 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 #Xem hướng dẫn tại : https://www.youtube.com/watch?v=VQQX6kAELb4
-$domain= 'http://nhutuit-001-site1.1tempurl.com'; // không có / ở cuối
-$linktoken= 'http://nhutuit-001-site1.1tempurl.com/nobita76_token/token.txt'; // nên để trong file txt nhé
+$domain= 'https://nhutdev.herokuapp.com'; // không có / ở cuối
+$linktoken= 'https://nhutdev.herokuapp.com/nobita76_token/token.txt'; // nên để trong file txt nhé
 $id_like= '100005371179208'; // ID User VIP
 $limitlike= '100000'; // Số Lượng Like/Status
 ?>
